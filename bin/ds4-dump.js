f885742411b937aded00a7a5189e018241c19499
Danny McClanahan <danieldmcclanahan@gmail.com>
2014-11-30 05:51:05 -0500

    updated ds4-dump to use sockets and animate onclick

--- bin/ds4-dump.js
+++ bin/ds4-dump.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+
 'use strict';
 
 var _ = require('lodash');
@@ -11,93 +12,98 @@ var fs = require('fs'),
   path = require("path");
 var Speaker = require('speaker');
 
-var io = require('socket.io')();
-io.on('connection', function(socket){});
-io.listen(3000);
+var app = require('express');
+var http = require('http').Server(app);
 
-var parseDS4HIDData = ds4.parseDS4HIDData;
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '../index.html');
+});
 
-var devices = hid.devices();
-var controller = _(devices).filter(isDS4HID).first();
+var io = require('socket.io')(http);
+io.on('connection', function(socket) {
+  console.log('a user connected');
 
-if (!controller) {
+  var parseDS4HIDData = ds4.parseDS4HIDData;
+
+  var devices = hid.devices();
+  var controller = _(devices).filter(isDS4HID).first();
+
+  if (!controller) {
     throw new Error('Could not find desired controller.');
-}
+  }
 
-var hidDevice = new hid.HID(controller.path);
-var offset = 0;
+  var hidDevice = new hid.HID(controller.path);
+  var offset = 0;
 
-if (isBluetoothHID(controller)) {
+  if (isBluetoothHID(controller)) {
     offset = 2;
     hidDevice.getFeatureReport(0x04, 66);
-}
+  }
 
+  //var soundSets =
+  var setList = [];
+  var trackSelected = 1;
 
-//var soundSets =
-var setList = [];
-var trackSelected = 1;
-
-var soundSets = fs.readdirSync("./sounds/").length - 1;
+  var soundSets = fs.readdirSync("./sounds/").length - 1;
   console.log(soundSets);
   //soundSets2 = f
-  
+
+  for (var i = 0; i < soundSets; i++) {
+    console.log("looped!");
+    console.log(i);
+    setList.push(i + 1);
+  }
+
+  var soundSet = setList[0];
+  console.log(setList);
+
+  /*BOOLEAN BUTTONS
+    dPadUp
+    dPadRight
+    dPadDown
+    dPadLeft
+    cross
+    circle
+    square
+    triangle
+    l1
+    l2
+    r1
+    r2
+    l3
+    r3
+    share
+    options
+    trackPadButton
+    psButton
+    trackPadTouch0Active
+    trackPadTouch1Active
+  */
+
+  var cache = {};
+
+  /*
+   * 0 = (not down)
+   * 2 = press
+   * 1 = release
+   * 3 = (down)
+   */
 
 
-
-for(var i = 0; i < soundSets; i++) { 
-  console.log("looped!");
-  console.log(i);
-  setList.push(i+1);
-}
-
-var soundSet = setList[0];
-console.log(setList);
+  function buttonPressed(button, state) {
+    var out = (state[button] << 1) | cache[button];
+    cache[button] = state[button];
+    return out;
+  }
 
 
-/*BOOLEAN BUTTONS
-  dPadUp
-  dPadRight
-  dPadDown
-  dPadLeft
-  cross
-  circle
-  square
-  triangle
-  l1
-  l2
-  r1
-  r2
-  l3
-  r3
-  share
-  options
-  trackPadButton
-  psButton
-  trackPadTouch0Active
-  trackPadTouch1Active
-*/
+  function playSound(soundNumber, set) {
+    play.sound('./sounds/' + set + '/' + soundNumber + ".wav");
+  }
 
-var cache = {};
 
-/*
- * 0 = (not down)
- * 2 = press
- * 1 = release
- * 3 = (down)
- */
-
-function buttonPressed(button, state) {
-	var out = (state[button] << 1) | cache[button];
-	cache[button] = state[button];
-	return out;
-}
-
-function playSound(soundNumber, set){
-  play.sound('./sounds/'+set+'/'+soundNumber+".wav");
-}
-
-function trackSelect(track){
-  /*if (track == trackSelected){
+  function trackSelect(track) {
+    /*if (track == trackSelected){
     if (trackSelected[mutedTrack]=true) {
       trackSelected[mutedTrack]=false;
       console.log("track "+track+" unmuted.");
@@ -106,114 +112,116 @@ function trackSelect(track){
       trackSelected[mutedTrack] = true;
       console.log("track "+track+" muted.");
     }
-      
+
   }
   else{*/
-    trackSelected=track;
-    console.log("track "+track+" selected.");
-}
+    trackSelected = track;
+    console.log("track " + track + " selected.");
+  }
 
-hidDevice.on('data', function(buf) {
-	var state = parseDS4HIDData(buf.slice(offset));
-	
-	var cross = buttonPressed("cross", state);
-	var circle = buttonPressed("circle", state);
-	var square = buttonPressed("square", state);
-  var triangle = buttonPressed("triangle", state);
-  var dPadUp = buttonPressed("dPadUp", state);
-  var dPadRight = buttonPressed("dPadRight", state);
-  var dPadDown = buttonPressed("dPadDown", state);
-  var dPadLeft = buttonPressed("dPadLeft", state);
-  var l1 = buttonPressed("l1", state);
-  var l2 = buttonPressed("l2", state);
-  var r1 = buttonPressed("r1", state);
-  var r2 = buttonPressed("r2", state);
-  var l3 = buttonPressed("l3", state);
-  var r3 = buttonPressed("r3", state);
-  var share = buttonPressed("share", state);
-  var options = buttonPressed("options", state);
-  var trackPadButton = buttonPressed("trackPadButton", state);
-  var psButton = buttonPressed("psButton", state);
-  var trackPadTouch0Active = buttonPressed("trackPadTouch0Active", state);
-  var trackPadTouch1Active = buttonPressed("trackPadTouch1Active", state);
+  hidDevice.on('data', function(buf) {
+    var state = parseDS4HIDData(buf.slice(offset));
 
-  if(r1==2){
-    //console.log("setlengt length -1" + setList[setList.length - 1]);
-    console.log("old soundset is " + soundSet);
-    if(soundSet ==setList.length){
-      soundSet=setList[0];
-      console.log("new soundset is 1");
+    var cross = buttonPressed("cross", state);
+    var circle = buttonPressed("circle", state);
+    var square = buttonPressed("square", state);
+    var triangle = buttonPressed("triangle", state);
+    var dPadUp = buttonPressed("dPadUp", state);
+    var dPadRight = buttonPressed("dPadRight", state);
+    var dPadDown = buttonPressed("dPadDown", state);
+    var dPadLeft = buttonPressed("dPadLeft", state);
+    var l1 = buttonPressed("l1", state);
+    var l2 = buttonPressed("l2", state);
+    var r1 = buttonPressed("r1", state);
+    var r2 = buttonPressed("r2", state);
+    var l3 = buttonPressed("l3", state);
+    var r3 = buttonPressed("r3", state);
+    var share = buttonPressed("share", state);
+    var options = buttonPressed("options", state);
+    var trackPadButton = buttonPressed("trackPadButton", state);
+    var psButton = buttonPressed("psButton", state);
+    var trackPadTouch0Active = buttonPressed("trackPadTouch0Active",
+      state);
+    var trackPadTouch1Active = buttonPressed("trackPadTouch1Active",
+      state);
+
+    if (r1 == 2) {
+      //console.log("setlengt length -1" + setList[setList.length - 1]);
+      console.log("old soundset is " + soundSet);
+      if (soundSet == setList.length) {
+        soundSet = setList[0];
+        console.log("new soundset is 1");
+      } else {
+        soundSet = setList[soundSet];
+        console.log("new soundset is " + soundSet);
+      }
+
     }
-    else{
-      soundSet=setList[soundSet];
-      console.log("new soundset is " + soundSet);
+
+    if (l1 == 2) {
+      console.log("old soundset is " + soundSet);
+      if (soundSet == 1) {
+        soundSet = setList.length;
+        console.log("new soundset is " + setList.length);
+      } else {
+        soundSet = setList[soundSet - 2];
+        console.log("new soundset is " + soundSet);
+      }
+
     }
-    
-  }
 
-  if(l1==2){
-    console.log("old soundset is " + soundSet);
-    if(soundSet==1){
-      soundSet=setList.length;
-      console.log("new soundset is "+setList.length);
-    }else{
-      soundSet=setList[soundSet-2];
-      console.log("new soundset is "+soundSet);
+    if (cross == 2) {
+      console.log("cross pressed.");
+      playSound(1, soundSet);
+      socket.emit("event_name", "LOL NERD");
     }
-    
-  }
+    if (cross == 1) {
+      console.log("cross released.");
+    }
 
-  if(cross==2){
-    console.log("cross pressed.");
-    playSound(1,soundSet);
-  }
-  if(cross==1){
-    console.log("cross released.");
-  }
+    if (circle == 2) {
+      console.log("circle pressed.");
+      playSound(2, soundSet);
+    }
+    if (circle == 1) {
+      console.log("cross released.");
+    }
 
-  if(circle==2){
-    console.log("circle pressed.");
-    playSound(2,soundSet);
-  }
-  if(circle==1){
-    console.log("cross released.");
-  }
+    if (triangle == 2) {
+      console.log("square pressed.");
+      playSound(3, soundSet);
+    }
+    if (triangle == 1) {
+      console.log("square released.");
+    }
 
-  if(triangle==2){
-    console.log("square pressed.");
-    playSound(3,soundSet);
-  }
-  if(triangle==1){
-    console.log("square released.");
-  }
+    if (square == 2) {
+      console.log("triangle pressed.");
+      playSound(4, soundSet);
+    }
+    if (square == 1) {
+      console.log("triangle released.");
+    }
 
-  if(square==2){
-    console.log("triangle pressed.");
-    playSound(4,soundSet);
-  }
-  if(square==1){
-    console.log("triangle released.");
-  }
-
-  if(dPadDown==2){
+    if (dPadDown == 2) {
       trackSelect("1");
-  }
-  if(dPadRight==2){
+    }
+    if (dPadRight == 2) {
       trackSelect("2");
-  }
-  if(dPadUp==2){
+    }
+    if (dPadUp == 2) {
       trackSelect("3");
-  }
-  if(dPadLeft==2){
+    }
+    if (dPadLeft == 2) {
       trackSelect("4");
-  }
+    }
 
-  var trackPadTouch0X = parseDS4HIDData(buf.slice(offset)).trackPadTouch0X;
-  var trackPadTouch0Y = parseDS4HIDData(buf.slice(offset)).trackPadTouch0Y;
-  var r2Analog = parseDS4HIDData(buf.slice(offset)).r2Analog;
+    var trackPadTouch0X = parseDS4HIDData(buf.slice(offset)).trackPadTouch0X;
+    var trackPadTouch0Y = parseDS4HIDData(buf.slice(offset)).trackPadTouch0Y;
+    var r2Analog = parseDS4HIDData(buf.slice(offset)).r2Analog;
 
-//if(r2==3){
-    console.log(r2Analog/255);
+    //if(r2==3){
+    console.log(r2Analog / 255);
 
     /*AAif(r2==3){
 
@@ -228,16 +236,19 @@ sin.set({freq:880});
     }
 //}
   if(trackPadTouch0Active){
-   // T("sin", {freq:400, mul:trackPadTouch0X}).play(); 
+   // T("sin", {freq:400, mul:trackPadTouch0X}).play();
     console.log(trackPadTouch0X+" / "+trackPadTouch0Y);
   }
 
 if(!trackPadTouch0Active){
-    //console.log("touchpad up");   
+    //console.log("touchpad up");
   }
 
 */
+  });
+
 });
+
 /*
   if (soundSet==1) {
     if(parseDS4HIDData(buf.slice(offset)).r1){
@@ -295,3 +306,7 @@ function isBluetoothHID(descriptor) {
 function isUSBHID(descriptor) {
   return descriptor.path.match(/^USB/);
 }
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+  });
